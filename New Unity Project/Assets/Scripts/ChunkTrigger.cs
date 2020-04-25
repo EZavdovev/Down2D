2d3ac38cb36fca5416db97e26c5f8975a41a878d
EvgeniySchur <laergjoll@gmail.com>
2020-04-25 17:39:16 +0200

    Update ChunkTrigger.cs

--- New Unity Project/Assets/Scripts/ChunkTrigger.cs
+++ New Unity Project/Assets/Scripts/ChunkTrigger.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class ChunkTrigger : MonoBehaviour
 {
+    private GameObject player; 
+
+    private void Awake() // Это один из способов. Мы ищем игрока через код. Лучше добавить игрока в инспекторе
+    {
+        player = GameObject.FindObjectOfType<Player>().gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            GameManager.PlayerFall.Publish("Fall");
+        }
+    }
+    
+    /*private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
             GameManager.PlayerFall.Publish("Fall");
         }
-    }
+    }*/
 }
